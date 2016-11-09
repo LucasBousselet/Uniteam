cd45680f4d2a28635c16688b74d05b9d7bb20ed5
Lucas Bousselet <LucasBousselet@gmail.com>
2016-11-09 18:06:58 -0500

    Adding of 4 mipmap button images of several sizes. Link from the project list to the presentation project page, with the actual project name displayed.

--- app/src/main/java/unidev/uniteam/ProjectList.java
+++ app/src/main/java/unidev/uniteam/ProjectList.java
@@ -1,5 +1,6 @@
 package unidev.uniteam;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -45,6 +47,23 @@ public class ProjectList extends AppCompatActivity {
 
                 //projectList.add("test");
                 //listAdapter.notifyDataSetChanged();
+            }
+        });
+
+        /* Defines the list of project as an OnItemCLickListener
+        So that we can call a event when an item of the list is clicked
+         */
+        projectListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?>adapter,View v, int position, long id){
+                //ItemClicked item = adapter.getItemAtPosition(position);
+
+                /* Intent used to start the activity ProjectPresentation */
+                Intent intent = new Intent(ProjectList.this,ProjectPresentation.class);
+                adapter.getItemAtPosition(position);
+                String nom = listAdapter.getItem(position);
+                intent.putExtra("ProjectName", nom);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/unidev/uniteam/ProjectPresentation.java
+++ app/src/main/java/unidev/uniteam/ProjectPresentation.java
@@ -1,7 +1,9 @@
 package unidev.uniteam;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProjectPresentation extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class ProjectPresentation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_presentation);
+
+        // We retrieve the intent used to create the activity (from the ProjectList page)
+        Intent intent = getIntent();
+        /* We fetch the name of the project that has been clicked previously, and pass through the intent
+        "ProjectName" is the name we gave the data when using the "putExtra" instruction */
+        String clickedProjectName = intent.getStringExtra("ProjectName");
+        // Then we retrieve the correct TextView and we set the new text inside it
+        TextView projectName = (TextView) findViewById(R.id.project_presentation_name);
+        projectName.setText(clickedProjectName);
     }
 }
