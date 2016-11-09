224a5506b5e7ac05548365bf68b590bd1e509757
Nicolas Gilbert <n-gilbert@laposte.net>
2016-11-08 21:50:35 -0500

    Add ProjectView page + AddProject page

--- app/src/main/java/unidev/uniteam/ProjectList.java
+++ app/src/main/java/unidev/uniteam/ProjectList.java
@@ -1,13 +1,20 @@
 package unidev.uniteam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ProjectList extends AppCompatActivity {
 
@@ -18,12 +25,26 @@ public class ProjectList extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        final ListView projectListView = (ListView) findViewById(R.id.project_list);
+        final ArrayList<String> projectList = new ArrayList<String>();
+        projectList.add("Test1");
+        projectList.add("Test2");
+
+        final ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, projectList);
+        projectListView.setAdapter(listAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Intent ProjectAdd = new Intent(ProjectList.this, ProjectAdd.class);
+                startActivity(ProjectAdd);
+
+                //projectList.add("test");
+                //listAdapter.notifyDataSetChanged();
             }
         });
     }
