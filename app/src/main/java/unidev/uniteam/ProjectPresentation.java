5fd145b4cc9875425c6da06b3b4b86dfa42e5844
Nicolas Gilbert <n-gilbert@laposte.net>
2016-11-18 23:34:58 -0500

    Add project fetch and send details (name + description) in database Add refresk button to reflesh the project list Add menu about in some activities (ProjectList, ProjectPresentation, TaskKanban) Add needed strings both in English and French Fix code warnings

--- app/src/main/java/unidev/uniteam/ProjectPresentation.java
+++ app/src/main/java/unidev/uniteam/ProjectPresentation.java
@@ -3,6 +3,8 @@ package unidev.uniteam;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -17,13 +19,17 @@ public class ProjectPresentation extends AppCompatActivity {
         // We retrieve the intent used to create the activity (from the ProjectList page)
         Intent intent = getIntent();
         /*
-        We fetch the name of the project that has been clicked previously, and pass through the intent
-        "ProjectName" is the name we gave the data when using the "putExtra" instruction
+         * We fetch the name of the Project that has been clicked previously, and pass through the intent
+         * *"ProjectName" is the name we gave the projectList when using the "putExtra" instruction
         */
         String clickedProjectName = intent.getStringExtra("ProjectName");
         // Then we retrieve the correct TextView and we set the new text inside it
         TextView projectName = (TextView) findViewById(R.id.project_presentation_name);
         projectName.setText(clickedProjectName);
+
+        String clickedProjectDescription = intent.getStringExtra("ProjectDescription");
+        TextView projectDescription = (TextView) findViewById(R.id.project_presentation_description);
+        projectDescription.setText(clickedProjectDescription);
 
         ImageButton taskButton = (ImageButton) findViewById(R.id.buttonProjectTasks);
         taskButton.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +40,21 @@ public class ProjectPresentation extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        if (item.getItemId() == R.id.action_about) {
+            Intent AboutPage = new Intent(ProjectPresentation.this, AboutUs.class);
+            startActivity(AboutPage);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
